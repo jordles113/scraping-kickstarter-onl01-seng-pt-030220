21117ff75327702d3645ad7eb703ead4ce3f721a
Jordan Romero <jordan.t.romero@gmail.com>
2020-04-07 19:18:35 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,9 +6,9 @@ require 'nokogiri'
 require 'pry'
 
 
-def create_project_hash	def create_project_hash
-  # write your code here	  html = File.read('fixtures/kickstarter.html')
-end 	  kickstarter = Nokogiri::HTML(html)
+def create_project_hash	
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
   projects = {}
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
